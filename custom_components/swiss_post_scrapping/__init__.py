498fe6ca70a4cc04423bee77974ee3b02b66fc7d
Nathan Bourquenoud <nathan.bourquenoud@edu.hefr.ch>
2024-08-22 15:02:16 +0200

    add async_setup_entry

--- custom_components/swiss_post_scrapping/__init__.py
+++ custom_components/swiss_post_scrapping/__init__.py
@@ -2,10 +2,28 @@ from homeassistant.helpers import discovery
 import logging
 from datetime import timedelta
 from homeassistant.helpers.event import track_time_interval
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
 DOMAIN = "swiss_post_scrapping"
 
 _LOGGER = logging.getLogger(__name__)
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Swiss Post Scrapping from a config entry."""
+    
+    # Charger la plateforme sensor
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
 
 def setup(hass, config):
     """Set up the My Selenium Integration."""
